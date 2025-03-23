345ef169917571a94da91892ebdf188e2e1332bf
Kenneth Nwoye <knwy@ymail.com>
2025-03-23 15:54:55 -0700

    responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./Header.css";
 import { Link, useNavigate } from "react-router-dom";
 import logoImage from "./images/transLogo2.png";
@@ -8,11 +8,44 @@ import { Dropdown, Menu } from "antd";
 const Header = () => {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleRef = useRef(null);
 
   const handleLogout = () => {
     setUser(null); // Clear the user
     navigate("/"); // Redirect to homepage
   };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      toggleRef.current &&
+      !toggleRef.current.contains(event.target)
+    ) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("touchstart", handleClickOutside); // Add for touch devices
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside); // Clean up
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [menuOpen]);
 
   const userMenu = (
     <Menu>
@@ -29,7 +62,14 @@ const Header = () => {
           <h1>StarFlicks Wiki</h1>
         </Link>
       </div>
-      <nav className="menu">
+
+      {/* Hamburger Menu Toggle */}
+      <div className="menu-toggle" onClick={toggleMenu} ref={toggleRef}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`menu ${menuOpen ? "open" : ""}`} ref={menuRef}>
         <Link to="/">Home</Link>
         <Link to="/seriesList?type=series">Series</Link>
         <Link to="/contact">Contact Us</Link>
@@ -47,9 +87,6 @@ const Header = () => {
             <Link to="/login">Login</Link>
           )}
         </div>
-        {/* <span role="img" aria-label="search">
-          🔍
-        </span> */}
       </div>
     </header>
   );
